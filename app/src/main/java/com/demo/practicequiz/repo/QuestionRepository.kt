c38a0191679aa3d964f2f6b9b40a59dd939c8e35
Pomog <thoryur@gmail.com>
2026-04-08 18:53:39 +0300

    Finishing up the Repository Class

--- app/src/main/java/com/demo/practicequiz/repo/QuestionRepository.kt
+++ app/src/main/java/com/demo/practicequiz/repo/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package com.demo.practicequiz.repo
 
+import android.util.Log
 import com.demo.practicequiz.data.DataOrException
 import com.demo.practicequiz.model.QuestionItem
 import com.demo.practicequiz.network.QuestionsAPI
@@ -8,8 +9,23 @@ import jakarta.inject.Inject
 class QuestionRepository @Inject constructor(
     private val api: QuestionsAPI
 ){
-    private val listOfQuestion
+    private val dataOrException
     = DataOrException<ArrayList<QuestionItem>,
     Boolean,
     Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception>{
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) {
+                dataOrException.loading = false
+            }
+        } catch (e: Exception) {
+            dataOrException.e = e
+            Log.d("Exception", "getAllQuestions: ${dataOrException.e?.localizedMessage}")
+        }
+
+        return dataOrException
+    }
 }
