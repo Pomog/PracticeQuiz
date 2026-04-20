b453f8acf03e60d2ff0aabb4bfd7dd920b57e1f4
Pomog <thoryur@gmail.com>
2026-04-20 22:32:08 +0300

    Radio Button Row & Checking

--- app/src/main/java/com/demo/practicequiz/component/Questions.kt
+++ app/src/main/java/com/demo/practicequiz/component/Questions.kt
@@ -1,15 +1,31 @@
 package com.demo.practicequiz.component
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -18,6 +34,7 @@ import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.demo.practicequiz.model.QuestionItem
 import com.demo.practicequiz.screens.QuestionsViewModel
 import com.demo.practicequiz.util.AppColors
 
@@ -32,8 +49,8 @@ fun Questions(
         CircularProgressIndicator()
         Log.d("Loading", "Questions are loading...")
     } else {
-        questions?.forEach { item ->
-            Log.d("Result", "Question = ${item.question}")
+        if (questions != null) {
+            QuestionDisplay(question = questions.first())
         }
         Log.d("SIZE", "size = ${questions?.size}")
     }
@@ -41,44 +58,154 @@ fun Questions(
 }
 
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    question: QuestionItem,
+    //questionIndex: MutableIntState,
+    //viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit = {}
+) {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 15f), 0f)
     Surface(
         modifier = Modifier
             .fillMaxSize()
             .padding(4.dp),
         color = AppColors.mDarkPurple
     ) {
+        val choicesState = remember(question) {
+            question.choices.toMutableList()
+        }
+
+        val answerState = remember(question) { mutableStateOf<Int?>(null) }
+        val correctAnswerState = remember(question) { mutableStateOf<Boolean?>(null) }
+
+        val updateAnswer: (Int) -> Unit = remember(question) {
+            {
+                answerState.value = it
+                correctAnswerState.value = choicesState[it] == question.answer
+            }
+        }
+
         Column(
             modifier = Modifier.padding(12.dp),
             verticalArrangement = androidx.compose.foundation.layout.Arrangement.Top,
             horizontalAlignment = androidx.compose.ui.Alignment.Start
         ) {
             QuestionTracker()
-        }
+            DrawDottedLine(pathEffect = pathEffect)
 
-    }
-}
-
-@Composable
-fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
-    Text(text = buildAnnotatedString {
-        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
-            withStyle(
-                style = SpanStyle(
-                    color = AppColors.mLightGray,
+            Column {
+                Text(
+                    text = question.question,
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(androidx.compose.ui.Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    fontSize = 17.sp,
+                    color = AppColors.mOffWhite,
                     fontWeight = FontWeight.Bold,
-                    fontSize = 27.sp
+                    lineHeight = 22.sp
                 )
-            ) {
-                append("Question $counter/")
-                withStyle(style = SpanStyle(color = AppColors.mLightGray,
-                    fontWeight = FontWeight.Light,
-                    fontSize = 14.sp)) {
-                    append("$outOf")
+
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        AppColors.mOffDarkPurple,
+                                        AppColors.mOffDarkPurple
+                                    )
+                                ),
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomStartPercent = 50,
+                                    bottomEndPercent = 50
+                                )
+                            )
+                            .background(Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (answerState.value == index),
+                            onClick = {
+                                updateAnswer(index)
+                            },
+                            modifier = Modifier.padding(start = 16.dp),
+                            colors = RadioButtonDefaults
+                                .colors(
+                                    selectedColor = if (
+                                        correctAnswerState.value == true
+                                        && index == answerState.value
+                                    ) {
+                                        Color.Green.copy(alpha = 0.2f)
+                                    } else {
+                                        Color.Red.copy(alpha = 0.2f)
+                                    }
+                                )
+                        ) // end RadioButton
+                        Text(text = answerText)
+                    }
                 }
             }
         }
-    },
-        modifier = Modifier.padding(20.dp)
-    )
+    }
 }
+
+
+
+
+    @Composable
+    fun DrawDottedLine(pathEffect: androidx.compose.ui.graphics.PathEffect) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp),
+        ) {
+            drawLine(
+                color = AppColors.mLightGray,
+                start = androidx.compose.ui.geometry.Offset(0f, 0f),
+                end = androidx.compose.ui.geometry.Offset(size.width, 0f),
+                pathEffect = pathEffect
+            )
+
+
+        }
+
+    }
+
+
+    @Composable
+    fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+                    withStyle(
+                        style = SpanStyle(
+                            color = AppColors.mLightGray,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 27.sp
+                        )
+                    ) {
+                        append("Question $counter/")
+                        withStyle(
+                            style = SpanStyle(
+                                color = AppColors.mLightGray,
+                                fontWeight = FontWeight.Light,
+                                fontSize = 14.sp
+                            )
+                        ) {
+                            append("$outOf")
+                        }
+                    }
+                }
+            },
+            modifier = Modifier.padding(20.dp)
+        )
+    }
